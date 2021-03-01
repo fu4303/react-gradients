0c66b1df58b0a36544066e5eb6ce481cabd4b54b
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-03-01 18:36:58 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Row, Col } from 'reactstrap'
 const Footer = () => (
   <Row className="Footer">
     <Col sm="12" className="text-center">
-      <p>made by <a rel="noopener noreferrer" href="https://github.com/estevanmaito" target="_blank">Estevan Maito</a></p>
+      <p>made by <a rel="noopener noreferrer" href="https://github.com/fu4303" target="_blank">Alec Campbell</a></p>
     </Col>
   </Row>
 )
